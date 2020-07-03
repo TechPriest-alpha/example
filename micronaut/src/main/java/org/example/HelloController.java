e4e2acbb9b8c3d32c4557be7e43dc6d79068e5a4
Viktor Denisov <VDenisov@luxoft.com>
2020-07-03 20:36:33 +0600

    application events

--- micronaut/src/main/java/org/example/HelloController.java
+++ micronaut/src/main/java/org/example/HelloController.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.micronaut.context.event.ApplicationEventPublisher;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -19,11 +20,13 @@ public class HelloController {
 
     private final HelperService svc;
     private final HelperService2 svc2;
+    private final ApplicationEventPublisher publisher;
 
     private final List<Api> apis;
 
     @Get(uri = "/hello/{name}", produces = MediaType.TEXT_PLAIN)
     public Single<String> hello(final @NotBlank String name) {
+        publisher.publishEvent(name);
         return Single.just("Hello " + name + "!" + svc.output() + " " + svc2.output() + " ?? " + apis.size());
     }
 
--- micronaut/src/main/java/org/example/HelperService.java
+++ micronaut/src/main/java/org/example/HelperService.java
@@ -1,12 +1,21 @@
 package org.example;
 
+import io.micronaut.runtime.event.annotation.EventListener;
 import io.micronaut.validation.Validated;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Singleton;
 
+@Slf4j
 @Singleton
 @Validated
 public class HelperService implements Api {
+
+    @EventListener
+    public void eventListener(final String event) {
+        log.info("Event: {}", event);
+    }
+
     public String output() {
         return "Wow11! ";
     }
