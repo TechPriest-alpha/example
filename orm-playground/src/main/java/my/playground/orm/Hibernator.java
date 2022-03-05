aaf602e2be3d5362f3e319c15edb449b749a962d
Viktor Denisov <VDenisov@luxoft.com>
2022-03-05 16:54:37 +0600

    minor cleanup

--- orm-playground/src/main/java/my/playground/orm/Hibernator.java
+++ orm-playground/src/main/java/my/playground/orm/Hibernator.java
@@ -1,6 +1,10 @@
 package my.playground.orm;
 
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.spi.BeanManager;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -13,20 +17,30 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 @ApplicationScoped
 public class Hibernator {
     private final StandardServiceRegistry standardRegistry;
-    private final Metadata metadata;
     private final SessionFactory sessionFactory;
+    private final EntityManager em;
 
-    public Hibernator() {
+    @Inject
+    public Hibernator(final BeanManager beanManager) {
         this.standardRegistry = new StandardServiceRegistryBuilder()
             .configure("/hibernate.cfg.xml")
             .build();
-        this.metadata = new MetadataSources(standardRegistry).buildMetadata();
-        this.sessionFactory = metadata.buildSessionFactory();
+        final Metadata metadata = new MetadataSources(standardRegistry).buildMetadata();
+        this.sessionFactory = metadata.getSessionFactoryBuilder().applyBeanManager(beanManager).build();
+        this.em = sessionFactory.createEntityManager();
+    }
+
+    @PreDestroy
+    public void stop() {
+        em.close();
+        sessionFactory.close();
+        standardRegistry.close();
+        log.info("Hibernator stopped");
     }
 
     public void getX() {
         try (final var s = sessionFactory.openSession()) {
-            log.info("Hi");
+            log.info("Hi: {}", em.isOpen());
         }
     }
 }
