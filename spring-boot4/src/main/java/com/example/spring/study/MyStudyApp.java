b4db91de5b65e3cd4020173223a3e9c2bd277a8d
Viktor Denisov <35419999+TechPriest-alpha@users.noreply.github.com>
2026-06-03 22:33:28 +0600

    skeleton for future spring-boot studies

---
+++ spring-boot4/src/main/java/com/example/spring/study/MyStudyApp.java
@@ -0,0 +1,11 @@
+package com.example.spring.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyStudyApp.class, args);
+    }
+}
---
+++ spring-boot4/src/test/java/com/example/spring/study/MyStudyAppTest.java
@@ -0,0 +1,12 @@
+package com.example.spring.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudyAppTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
